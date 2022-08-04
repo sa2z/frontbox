a984c87196048293bea2ca497fef251c9c902196
sa2z <57865727+sa2z@users.noreply.github.com>
2022-08-04 16:47:12 +0900

    minor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import React, { useState } from "react";
-import { IntlProvider } from "react-intl";
-import Layout from "./Layout";
-import messages from "./messages";
-import {
-  ProSidebar,
-  SidebarHeader,
-  SidebarFooter,
-  SidebarContent,
-  Menu,
-  MenuItem,
-  SubMenu
-} from "react-pro-sidebar";
+// import React, { useState } from "react";
+// import { IntlProvider } from "react-intl";
+// import Layout from "./Layout";
+// import messages from "./messages";
+// import {
+//   ProSidebar,
+//   SidebarHeader,
+//   SidebarFooter,
+//   SidebarContent,
+//   Menu,
+//   MenuItem,
+//   SubMenu
+// } from "react-pro-sidebar";
 
-export default function App() {
-  const [locale, setLocale] = useState("en");
+// export default function App() {
+//   const [locale, setLocale] = useState("en");
 
-  return (
-    <IntlProvider locale={locale} messages={messages[locale]}>
-      <Layout setLocale={setLocale} />
-    </IntlProvider>
-  );
+//   return (
+//     <IntlProvider locale={locale} messages={messages[locale]}>
+//       <Layout setLocale={setLocale} />
+//     </IntlProvider>
+//   );
+// }
+import React from "react";
+import styled from "styled-components";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import MyList from "./pages/MyList";
+import LikedList from "./pages/LikedList";
+import Setting from "./pages/Setting";
+
+const Center = styled.div`
+  height: 92vh;
+  display: flex;
+  flex-direction: row;
+`;
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <Center>
+          <Sidebar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/mylist" component={MyList} />
+            <Route path="/likedlist" component={LikedList} />
+            <Route path="/setting" component={Setting} />
+          </Switch>
+        </Center>
+      </BrowserRouter>
+    );
+  }
 }
+
+export default App;
--- src/messages.js
+++ src/messages.js
@@ -16,5 +16,23 @@ export default {
     withPrefix: "With Prefix",
     withSuffix: "With Suffix",
     viewSource: "View Source"
+  },
+  ar: {
+    title: "رياكت پرو سايد بار",
+    sidebarTitle: "پرو سايد بار",
+    description:
+      "شريط جانبي مع قوائم منسدلة وعدد غير محدود من القوائم الفرعية المتداخلة",
+    dashboard: "لوحة القيادة",
+    components: "مكونات",
+    dropdown: "اسقاط",
+    submenu: "قائمة فرعية",
+    multiLevel: "متعدد المستويات",
+    collapsed: "انطوى",
+    rtl: "ر ت ل",
+    image: "صورة الخلفية",
+    new: "جديد",
+    withPrefix: "مع البادئة",
+    withSuffix: "مع اللاحقة",
+    viewSource: "عرض الكود"
   }
 };
